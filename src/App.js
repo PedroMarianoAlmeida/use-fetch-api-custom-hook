619c7f943a46507a0755490ae3238058db632197
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2020-06-17 00:19:47 -0300

    Error Handling

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 import useAPI from './useAPI';
 
+//Based on: https://medium.com/swlh/usefetch-a-custom-react-hook-36d5f5819d8
+
 function App() {
   
   const url = 'https://api.nationalize.io?name=michael'
-  const { data, isLoading } = useAPI(url)
+  const { data, isLoading, hasError, errorMessage } = useAPI(url)
 
   return (
     <div className="App">
       
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {isLoading? <h1>...loading</h1> : <h1>{data}</h1>}
+        {hasError? <h1>{errorMessage}</h1> : (isLoading? <h1>...loading</h1> : <h1>{data}</h1>)}
       </header>
     </div>
   );
--- src/useAPI.js
+++ src/useAPI.js
@@ -2,19 +2,31 @@ import { useState, useEffect } from 'react'
 const useAPI = (url) => {
   const [data, setData] = useState(null)
   const [isLoading, setIsLoading] = useState(false)
+  const [hasError, setHasError] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('')
 
 useEffect(() => {
     const fetchData = async () => {
         setIsLoading(true)
-        fetch(url)
-        .then((response) => response.json())
-        .then((result) => {
-          setData(result.name)
-          setIsLoading(false)
-        })
-      }
+        try {
+            const response = await fetch(url)
+            const result = await response.json()
+            if (response.ok) {
+              setData(result.name)
+            } else {
+              setHasError(true)
+              setErrorMessage(`Error: ${result.error}`)
+              console.log(result)
+            }
+            setIsLoading(false)
+          } catch (err) {
+            setHasError(true)
+            setErrorMessage(err.message)
+            setIsLoading(false)
+          }
+        }
       fetchData()
     }, [])
-return { data, isLoading }
+return { data, isLoading, hasError, errorMessage }
   }
 export default useAPI
